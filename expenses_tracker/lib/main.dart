def9d20ce682fcec69064d9ab1d2d167c8b0b3f8
acatarinaoaraujo <acoa_@hotmail.com>
2022-08-11 16:49:35 -1000

    title

--- expenses_tracker/lib/main.dart
+++ expenses_tracker/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Personal Expenses',
+      title: 'My Weekly Expenses',
       theme: ThemeData(
           primarySwatch: Colors.purple,
           accentColor: Colors.amber,
