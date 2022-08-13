e96f357016f0225829bce58da89bbc7da86328cb
acatarinaoaraujo <acoa_@hotmail.com>
2022-08-12 21:34:19 -1000

    new updates

--- expenses_tracker/lib/widgets/chart_bar.dart
+++ expenses_tracker/lib/widgets/chart_bar.dart
@@ -17,39 +17,45 @@ class ChartBar extends StatelessWidget {
         SizedBox(
           width: 10,
         ),
-        Container(
-          height: 16,
-          width: 100,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey, width: 1.0),
-                  color: Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              FractionallySizedBox(
-                widthFactor: spendingPctOfTotal,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(10),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 16,
+              width: 240,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1.0),
+                      color: Color.fromRGBO(220, 220, 220, 1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
-                ),
+                  FractionallySizedBox(
+                    widthFactor: spendingPctOfTotal,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            Container(
+              height: 20,
+              child: FittedBox(
+                child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+              ),
+            ),
+          ],
         ),
         SizedBox(
           width: 10,
         ),
-        Container(
-          height: 20,
-          child: FittedBox(
-            child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
-          ),
-        ),
+        Align(alignment: Alignment(-0.7, -0.3), child: Text(label)),
       ],
     );
   }
